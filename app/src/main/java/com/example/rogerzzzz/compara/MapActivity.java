ad9036c30051da4ee3ec78aec30ad08f60090bda
rogerzzzzz <501841352@qq.com>
2016-12-08 16:01:23 +0800

    add map

--- app/src/main/java/com/example/rogerzzzz/compara/MapActivity.java
+++ app/src/main/java/com/example/rogerzzzz/compara/MapActivity.java
@@ -1,11 +1,12 @@
 package com.example.rogerzzzz.compara;
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
@@ -15,51 +16,67 @@ import com.amap.api.maps2d.AMap;
 import com.amap.api.maps2d.CameraUpdateFactory;
 import com.amap.api.maps2d.LocationSource;
 import com.amap.api.maps2d.MapView;
-import com.amap.api.maps2d.model.Circle;
+import com.amap.api.maps2d.model.BitmapDescriptor;
+import com.amap.api.maps2d.model.BitmapDescriptorFactory;
+import com.amap.api.maps2d.model.CircleOptions;
 import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.maps2d.model.LatLngBounds;
 import com.amap.api.maps2d.model.Marker;
-import com.example.rogerzzzz.compara.common.utils.SensorEventHelper;
+import com.amap.api.maps2d.model.MarkerOptions;
+import com.amap.api.maps2d.model.MyLocationStyle;
+import com.amap.api.services.core.AMapException;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.core.PoiItem;
+import com.amap.api.services.core.SuggestionCity;
+import com.amap.api.services.poisearch.PoiResult;
+import com.amap.api.services.poisearch.PoiSearch;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by rogerzzzz on 2016/12/3.
  */
 
 public class MapActivity extends AppCompatActivity implements LocationSource,
-        AMapLocationListener {
+        AMapLocationListener, PoiSearch.OnPoiSearchListener {
     private MapView                   mapView;
     private AMap                      aMap;
     private OnLocationChangedListener mListener;
     private AMapLocationClient        mlocationClient;
     private AMapLocationClientOption  mLocationOption;
-    private Marker                    mLocMarker;
-    private SensorEventHelper         mSensorHelper;
-    private boolean mFirstFix = false;
-    private Circle mCircle;
+    private boolean                   firstInit = true;
+    public static final String LOCATION_MARKER_FLAG = "mylocation";
+    private PoiSearch poiSearch;
+    private PoiResult poiResult; // poi result detail
+    private int currentPage = 0;
+    private PoiSearch.Query query;// Query Class
+    private LatLonPoint lp = null;
+    private String city = "香港";
+    private String        supermarketName;
+    private List<PoiItem> poiItems;
+    private myPoiOverlay    poiOverlay;
+    private Marker mlastMarker;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_activity);
-        Intent intent = getIntent();
-        String name = intent.getStringExtra("supermarket_name");
-        Log.i("--->22", name);
-        initMap(savedInstanceState);
-    }
-
-    private void initMap(Bundle savedInstanceState){
         mapView = (MapView) findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        supermarketName = intent.getStringExtra("supermarket_name");
+        initMap();
+    }
+
+    private void initMap(){
         if (aMap == null) {
             aMap = mapView.getMap();
-            aMap.setLocationSource(this);// 设置定位监听
-            aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
-            aMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+            aMap.setLocationSource(this);// set listener
+            aMap.getUiSettings().setMyLocationButtonEnabled(true);
+            aMap.setMyLocationEnabled(true);
+            setupLocationStyle();
         }
-        mSensorHelper = new SensorEventHelper(this);
-        if (mSensorHelper != null) {
-            mSensorHelper.registerSensorListener();
-        }
-
     }
 
     @Override
@@ -81,58 +98,247 @@ public class MapActivity extends AppCompatActivity implements LocationSource,
     protected void onPause() {
         super.onPause();
         mapView.onPause();
-        if (mSensorHelper != null) {
-            mSensorHelper.unRegisterSensorListener();
-            mSensorHelper.setCurrentMarker(null);
-            mSensorHelper = null;
-        }
+        deactivate();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         mapView.onResume();
-        if (mSensorHelper != null) {
-            mSensorHelper.registerSensorListener();
-        }
-
     }
+
+    private void setupLocationStyle(){
+        MyLocationStyle myLocationStyle = new MyLocationStyle();
+        myLocationStyle.myLocationIcon(BitmapDescriptorFactory.
+                fromResource(R.drawable.point4));
+        aMap.setMyLocationStyle(myLocationStyle);
+    }
+
 
     @Override
     public void onLocationChanged(AMapLocation amapLocation) {
         if (mListener != null && amapLocation != null) {
             if (amapLocation != null
                     && amapLocation.getErrorCode() == 0) {
-//                mLocationErrText.setVisibility(View.GONE);
-                LatLng location = new LatLng(amapLocation.getLatitude(), amapLocation.getLongitude());
-                if (!mFirstFix) {
-                    mFirstFix = true;
-                    addCircle(location, amapLocation.getAccuracy());//添加定位精度圆
-                    addMarker(location);//添加定位图标
-                    mSensorHelper.setCurrentMarker(mLocMarker);//定位图标旋转
-                } else {
-                    mCircle.setCenter(location);
-                    mCircle.setRadius(amapLocation.getAccuracy());
-                    mLocMarker.setPosition(location);
+                mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
+                if(firstInit){
+                    lp = new LatLonPoint(amapLocation.getLatitude(), amapLocation.getLongitude());
+                    doSearchQuery(supermarketName);
+                    aMap.moveCamera(CameraUpdateFactory.zoomTo(18));
+                    firstInit = false;
                 }
-                aMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 18));
             } else {
                 String errText = "定位失败," + amapLocation.getErrorCode()+ ": " + amapLocation.getErrorInfo();
                 Log.e("AmapErr",errText);
-                mLocationErrText.setVisibility(View.VISIBLE);
-                mLocationErrText.setText(errText);
             }
         }
+    }
 
+    protected void doSearchQuery(String keyWord) {
+        query = new PoiSearch.Query(keyWord, "", city);
+        query.setPageSize(3);
+        query.setPageNum(currentPage);
+
+        if (lp != null) {
+            poiSearch = new PoiSearch(this, query);
+            poiSearch.setOnPoiSearchListener(this);
+            PoiSearch.SearchBound bound = new PoiSearch.SearchBound(lp, 10000, true);
+            poiSearch.setBound(bound);//
+            poiSearch.searchPOIAsyn();// 异步搜索
+        }
     }
 
     @Override
     public void activate(OnLocationChangedListener onLocationChangedListener) {
-
+        mListener = onLocationChangedListener;
+        if (mlocationClient == null) {
+            mlocationClient = new AMapLocationClient(this);
+            mLocationOption = new AMapLocationClientOption();
+            mlocationClient.setLocationListener(this);
+            //set the mode
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            //set parameters
+            mlocationClient.setLocationOption(mLocationOption);
+            mlocationClient.startLocation();
+        }
     }
 
     @Override
     public void deactivate() {
+        mListener = null;
+        if (mlocationClient != null) {
+            mlocationClient.stopLocation();
+            mlocationClient.onDestroy();
+        }
+        mlocationClient = null;
+    }
 
+    @Override
+    public void onPoiSearched(PoiResult result, int rcode) {
+        if (rcode == AMapException.CODE_AMAP_SUCCESS) {
+            if (result != null && result.getQuery() != null) {
+                if (result.getQuery().equals(query)) {// check if it is same
+                    poiResult = result;
+                    poiItems = poiResult.getPois();
+                    List<SuggestionCity> suggestionCities = poiResult
+                            .getSearchSuggestionCitys();
+                    if (poiItems != null && poiItems.size() > 0) {
+                        if (mlastMarker != null) {
+                            resetlastmarker();
+                        }
+                        if (poiOverlay !=null) {
+                            poiOverlay.removeFromMap();
+                        }
+                        aMap.clear();
+                        poiOverlay = new myPoiOverlay(aMap, poiItems);
+                        poiOverlay.addToMap();
+                        poiOverlay.zoomToSpan();
+
+                        aMap.addMarker(new MarkerOptions()
+                                .anchor(0.5f, 0.5f)
+                                .icon(BitmapDescriptorFactory
+                                        .fromBitmap(BitmapFactory.decodeResource(
+                                                getResources(), R.drawable.point4)))
+                                .position(new LatLng(lp.getLatitude(), lp.getLongitude())));
+
+                        aMap.addCircle(new CircleOptions()
+                                .center(new LatLng(lp.getLatitude(),
+                                        lp.getLongitude())).radius(5000)
+                                .strokeColor(Color.BLUE)
+                                .fillColor(Color.argb(50, 1, 1, 1))
+                                .strokeWidth(2));
+                    } else if (suggestionCities != null
+                            && suggestionCities.size() > 0) {
+                    } else {
+                        Log.i("--->1", "error1");
+                    }
+                }
+            } else {
+                Log.i("--->1", "error2");
+            }
+        } else {
+            Log.i("--->1", "error3");
+        }
+    }
+
+    @Override
+    public void onPoiItemSearched(PoiItem poiItem, int i) {
+
+    }
+
+    private int[] markers = {R.drawable.poi_marker_1,
+            R.drawable.poi_marker_2,
+            R.drawable.poi_marker_3,
+            R.drawable.poi_marker_4,
+            R.drawable.poi_marker_5,
+            R.drawable.poi_marker_6,
+            R.drawable.poi_marker_7,
+            R.drawable.poi_marker_8,
+            R.drawable.poi_marker_9,
+            R.drawable.poi_marker_10
+    };
+
+    private void resetlastmarker() {
+        int index = poiOverlay.getPoiIndex(mlastMarker);
+        if (index < 10) {
+            mlastMarker.setIcon(BitmapDescriptorFactory
+                    .fromBitmap(BitmapFactory.decodeResource(
+                            getResources(),
+                            markers[index])));
+        }else {
+            mlastMarker.setIcon(BitmapDescriptorFactory.fromBitmap(
+                    BitmapFactory.decodeResource(getResources(), R.drawable.marker_other_highlight)));
+        }
+        mlastMarker = null;
+
+    }
+
+    private class myPoiOverlay {
+        private AMap mamap;
+        private List<PoiItem> mPois;
+        private ArrayList<Marker> mPoiMarks = new ArrayList<Marker>();
+        public myPoiOverlay(AMap amap ,List<PoiItem> pois) {
+            mamap = amap;
+            mPois = pois;
+        }
+
+        public void addToMap() {
+            for (int i = 0; i < mPois.size(); i++) {
+                Marker marker = mamap.addMarker(getMarkerOptions(i));
+                PoiItem item = mPois.get(i);
+                marker.setObject(item);
+                mPoiMarks.add(marker);
+            }
+        }
+
+        public void removeFromMap() {
+            for (Marker mark : mPoiMarks) {
+                mark.remove();
+            }
+        }
+
+        public void zoomToSpan() {
+            if (mPois != null && mPois.size() > 0) {
+                if (mamap == null)
+                    return;
+                LatLngBounds bounds = getLatLngBounds();
+                mamap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100));
+            }
+        }
+
+        private LatLngBounds getLatLngBounds() {
+            LatLngBounds.Builder b = LatLngBounds.builder();
+            for (int i = 0; i < mPois.size(); i++) {
+                b.include(new LatLng(mPois.get(i).getLatLonPoint().getLatitude(),
+                        mPois.get(i).getLatLonPoint().getLongitude()));
+            }
+            return b.build();
+        }
+
+        private MarkerOptions getMarkerOptions(int index) {
+            return new MarkerOptions()
+                    .position(
+                            new LatLng(mPois.get(index).getLatLonPoint()
+                                    .getLatitude(), mPois.get(index)
+                                    .getLatLonPoint().getLongitude()))
+                    .title(getTitle(index)).snippet(getSnippet(index))
+                    .icon(getBitmapDescriptor(index));
+        }
+
+        protected String getTitle(int index) {
+            return mPois.get(index).getTitle();
+        }
+
+        protected String getSnippet(int index) {
+            return mPois.get(index).getSnippet();
+        }
+
+        public int getPoiIndex(Marker marker) {
+            for (int i = 0; i < mPoiMarks.size(); i++) {
+                if (mPoiMarks.get(i).equals(marker)) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public PoiItem getPoiItem(int index) {
+            if (index < 0 || index >= mPois.size()) {
+                return null;
+            }
+            return mPois.get(index);
+        }
+
+        protected BitmapDescriptor getBitmapDescriptor(int arg0) {
+            if (arg0 < 10) {
+                BitmapDescriptor icon = BitmapDescriptorFactory.fromBitmap(
+                        BitmapFactory.decodeResource(getResources(), markers[arg0]));
+                return icon;
+            }else {
+                BitmapDescriptor icon = BitmapDescriptorFactory.fromBitmap(
+                        BitmapFactory.decodeResource(getResources(), R.drawable.marker_other_highlight));
+                return icon;
+            }
+        }
     }
 }
